dc91a365a98727d4af4deb904678ea1e5539f9da
xandeezx <arthurmonte34@gmail.com>
2026-04-23 19:41:50 -0300

    config mongo

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config()
 
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
-app.use(cors());
-app.use(express.json());
+const app = express()
 
-app.get('/', (req, res) => res.json({ message: 'API do App Bus funcionando.' }));
+app.use(cors())
+app.use(express.json())
 
-app.use('/api/linhas', require('./routes/linhaRoutes'));
-app.use('/api/paradas', require('./routes/paradaRoutes'));
+app.get('/', (req, res) => {
+  res.json({ message: 'API do App Bus funcionando.' })
+})
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/app_bus')
-  .then(() =>
-    app.listen(process.env.PORT || 3000, () =>
-      console.log('Servidor rodando')
-    )
-  );
+app.use('/api/linhas', require('./routes/linhaRoutes'))
+app.use('/api/paradas', require('./routes/paradaRoutes'))
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Mongo conectado')
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`)
+    })
+  })
+  .catch(err => console.log(err))
